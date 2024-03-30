d6b597f65ff6a45bcb3c2fe0499c657376482b01
Cooper Marshall <cooperqmarshall@gmail.com>
2024-03-29 19:32:55 -0700

    add recipe api library

--- api/recipe.api.go
+++ api/recipe.api.go
@@ -3,10 +3,13 @@ package api
 import (
 	"fmt"
 	"htmx-test/db"
+	"io"
 	"net/http"
 	"strconv"
 
+	"github.com/cooperqmarshall/recipe"
 	"github.com/labstack/echo/v4"
+	"golang.org/x/net/html"
 )
 
 func (handler Handler) PostRecipe(c echo.Context) error {
@@ -15,12 +18,44 @@ func (handler Handler) PostRecipe(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	err := db.PostRecipe(handler.DB, r)
+    html, err := fetch_recipe_html(r.Url)
+    if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+    }
+
+    _, err = extract_recipe_ldjson(html)
+    _ = new(recipe.Recipe)
+    // r.Read_jsonld()
+    if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+    }
+
+	err = db.PostRecipe(handler.DB, r)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("%b", err))
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
 	return c.Render(http.StatusOK, "add_recipe_result", r)
+}
+
+func fetch_recipe_html(url string) (io.Reader, error) {
+    res, err := http.Get(url)
+    if err != nil {
+        return nil, err
+    }
+    return res.Body, nil
+}
+
+func extract_recipe_ldjson(r io.Reader) ([]byte, error) {
+    t := html.NewTokenizer(r)
+
+    for {
+        if t.Next() == html.ErrorToken {
+            return nil, t.Err()
+        }
+        fmt.Printf("%s", t.Token())
+        return nil, nil
+    }
 }
 
 func (handler Handler) GetRecipe(c echo.Context) error {
