ed98627f3586bea3c2d32751443adee66b5c86d3
Cooper Marshall <cooperqmarshall@gmail.com>
2024-06-02 13:01:45 -0700

    fix(list-search): Localized javascript to list search and selection

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,3 @@
-function handelListSearchResultClick(e) {
-  let list = e.srcElement.innerText
-  document.getElementById('list-input').value = list
-}
-
 // const listInputElement = document.getElementById('list-input')
 // listInputElement.addEventListener('focus', () => {
 //   const listDropdownElement = document.getElementById('list-dropdown')
