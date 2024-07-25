0dafa4cfc297ccbf0ee5368e366654145000ea06
Cooper Marshall <cooperqmarshall@gmail.com>
2024-07-24 21:17:27 -0700

    Added custom color scheme and gradient for recipe photo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./templates/*/*.html"],
   theme: {
     extend: {},
+    colors: {
+        "transparent": "transparent",
+        "current": "currentColor",
+        "white": colors.white,
+        "indigo": colors.indigo,
+        "gray": colors.gray,
+        "turquoise": "#2dd4bf",
+        "jordy-blue": "#93c5fd",
+        "mauve": {100: "#d9c2f2", 200: "#d8b4fe", 300: "#c29de9"},
+        "carnation-pink": "#fe99cf",
+        "seasalt": "#fafafa",
+    },
   },
   plugins: [],
 }
